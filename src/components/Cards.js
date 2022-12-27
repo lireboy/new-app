6170282c9f1c2401e34d6a4b64c3e2d48453af78
TIMURSPC\timur <timur.oez@web.de>
2022-12-27 12:09:00 +0100

    - add link placeholders in Cards - add hover effect on image - remove white background on images

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,20 +1,23 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import '../css/Cards.css';
+import {Link} from 'react-router-dom';
 
 function Cards() {
   const cardInfo = [
-    {image: "https://upload.wikimedia.org/wikipedia/commons/9/93/Wordpress_Blue_logo.png", title: "wordpress", text: "Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content."},
-    {image: "https://ibbf.berlin/assets/images/logos/qlee.png", title: "qlee", text: "Some quick example text to build on the card title and make up the bulk of the card's content."}
+    {image: "https://upload.wikimedia.org/wikipedia/commons/9/93/Wordpress_Blue_logo.png", title: "wordpress", text: "Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.", link: "https://wordpress.com/de/"},
+    {image: "https://ibbf.berlin/assets/images/logos/qlee.png", title: "qlee", text: "Some quick example text to build on the card title and make up the bulk of the card's content.", link: "https://qlee.eu/"}
   ];
   const renderCard = (card, index) =>{
     return(
       <Card style={{ width: '20rem' }} key={index}>
-      <Card.Img variant="top" src={card.image} />
+        <a href={card.link} target="_blank" rel='noopener noreferrer'>
+          <Card.Img variant="top" src={card.image}></Card.Img>
+        </a>
       <Card.Body>
         <Card.Title>{card.title}</Card.Title>
         <Card.Text>{card.text}</Card.Text>
-        <Button variant="primary">Go to project</Button>
+        <Button variant="primary" href={card.link} target="_blank">Go to project</Button>
       </Card.Body>
     </Card>
     )
